b27bf39cf95ca1a468c79a3c4d7bc10add323fd8
Flpichon <33513602+Flpichon@users.noreply.github.com>
2017-11-16 12:19:10 +0100

    3

--- exercice1.php
+++ exercice1.php
@@ -1,5 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 <?php
-
 $bateaux=array (
     "Manureva" => array("nbequipage"=>3,"nbpassager"=>4),
     "Sirius"=> array("nbequipage"=>4,"nbpassager"=>6),
@@ -16,3 +24,7 @@ echo $infoequipage["nbpassager"].'<br>';
 //echo $bateaux["Manureva"]["nbpassager"];
 
 ?>
+</body>
+</html>
+
+
